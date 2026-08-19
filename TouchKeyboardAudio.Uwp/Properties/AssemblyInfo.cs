0f4fd6020df50ceec9091bc783f388421bfe7cc4
JYUSHITI <17yizuka@gmail.com>
2026-08-19 12:10:00 +0800

    Add UWP assembly metadata

---
+++ TouchKeyboardAudio.Uwp/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TouchKeyboardAudio.Uwp")]
+[assembly: AssemblyDescription("Native UWP frontend for TouchKeyboardAudio")]
+[assembly: AssemblyCompany("Yizuka17")]
+[assembly: AssemblyProduct("TouchKeyboardAudio")]
+[assembly: AssemblyCopyright("Copyright © Yizuka17 2026")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
